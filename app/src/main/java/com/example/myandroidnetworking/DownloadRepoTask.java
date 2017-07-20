39b1ca8feeb575d0545825276047c5ccadbad5d2
DarushDev <darushfaqiri@gmail.com>
2017-07-20 16:54:16 +0800

    Connect to url

--- app/src/main/java/com/example/myandroidnetworking/DownloadRepoTask.java
+++ app/src/main/java/com/example/myandroidnetworking/DownloadRepoTask.java
@@ -2,6 +2,11 @@ package com.example.myandroidnetworking;
 
 import android.os.AsyncTask;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by Lenovo Desktop on 7/20/2017.
  */
@@ -10,12 +15,34 @@ public class DownloadRepoTask extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
-        return null;
+
+        try {
+            return downloadData(params[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @Override
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
+    }
+
+    private String downloadData (String urlString) throws IOException {
+        InputStream is = null;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            return null;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
     }
 
 }
