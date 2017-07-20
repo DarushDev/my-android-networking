017ae590be64541d78ac2cf9bfefbee9662ebd6e
DarushDev <darushfaqiri@gmail.com>
2017-07-20 17:04:19 +0800

    Convert input stream to string, add download complete listener interface

---
+++ app/src/main/java/com/example/myandroidnetworking/DownloadCompleteListener.java
@@ -0,0 +1,13 @@
+package com.example.myandroidnetworking;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lenovo Desktop on 7/20/2017.
+ */
+
+public interface DownloadCompleteListener {
+
+    void downloadComplete(ArrayList<Repository> repositories);
+
+}
--- app/src/main/java/com/example/myandroidnetworking/DownloadRepoTask.java
+++ app/src/main/java/com/example/myandroidnetworking/DownloadRepoTask.java
@@ -2,8 +2,10 @@ package com.example.myandroidnetworking;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -37,12 +39,25 @@ public class DownloadRepoTask extends AsyncTask<String, Void, String> {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
-            return null;
+            is = conn.getInputStream();
+
+            return convertToString(is);
         } finally {
             if (is != null) {
                 is.close();
             }
         }
+    }
+
+    private String convertToString(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder string = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            string.append(line);
+        }
+        return string.toString();
+
     }
 
 }
